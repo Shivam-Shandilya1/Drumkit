d7a458f6048e001e780a1b270a12cf0cb4573d94
Shivam-Shandilya1 <shivamshandilya12@gmail.com>
2021-02-28 08:49:12 +0530

    files commited

---
+++ index.js
@@ -0,0 +1,41 @@
+var drmnum = document.querySelector(".btn").length;
+for(var i=0;i<drmnum;i++)
+{
+    document.querySelector(".btn")[i].addEventListener("click",function()
+{
+    this.innerHTML(style="color:white;");
+})
+}
+
+/**var audio = new Audio ('sounds/tom-1.mp3');
+    audio.play();**/
+/******document.querySelector(".druma").addEventListener("click",function()
+{
+    var audio = new Audio ('sounds/tom-2.mp3');
+    audio.play();
+})
+document.querySelector(".drums").addEventListener("click",function()
+{
+    var audio = new Audio ('sounds/tom-3.mp3');
+    audio.play();
+})
+document.querySelector(".drumd").addEventListener("click",function()
+{
+    var audio = new Audio ('sounds/tom-4.mp3');
+    audio.play();
+})
+document.querySelector(".drumj").addEventListener("click",function()
+{
+    var audio = new Audio ('sounds/snare.mp3');
+    audio.play();
+})
+document.querySelector(".drumk").addEventListener("click",function()
+{
+    var audio = new Audio ('sounds/crash.mp3');
+    audio.play();
+})
+document.querySelector(".druml").addEventListener("click",function()
+{
+    var audio = new Audio ('sounds/kick-bass.mp3');
+    audio.play();
+})/******
